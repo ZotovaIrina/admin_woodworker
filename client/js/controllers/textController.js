2402d12011180601090bcaaa79f97e9b131d20e3
ZotovaIrina <ZotovaIrina@users.noreply.github.com>
2016-02-18 14:34:36 +0300

    Debugging get data.

--- client/js/controllers/textController.js
+++ client/js/controllers/textController.js
@@ -1,27 +1,18 @@
 var app = require("../app");
 
-app.controller('TextController', ['$scope', '$stateParams', 'textService', '$http', function ($scope, $stateParams, textService, $http) {
+app.controller('TextController', ['$scope', '$stateParams', 'textService', function ($scope, $stateParams, textService) {
 
     var id = $stateParams.id;
     var fileAddress = "resource/template/" + id + ".html";
-    console.log (fileAddress);
-    $http.get(fileAddress).then(function(response) {
-        $scope.rawHtml = response.data;
-        console.log($scope.rawHtml);
-        console.log('type rawHtml ', typeof $scope.rawHtml);
-    });
+    textService.getHtml(fileAddress)
+        .then(function(response) {
+            $scope.rawHtml = response;
+                console.log($scope.rawHtml);
+            console.log('type rawHtml ', typeof $scope.rawHtml);
+        });
 
-    //textService.getHtml().get({id: id}).$promise
-    //    .then(function (response) {
-    //        $scope.orightml =response;
-    //            console.log($scope.orightml);
-    //        console.log('type orightml ', typeof $scope.orightml);
-    //    },
-    //    function (response) {
-    //        $scope.message = "Error: " + response.status + " " + response.statusText;
-    //    }
-    //
-    //);
+
+
 
 
 
--- client/js/services/textService.js
+++ client/js/services/textService.js
@@ -1,19 +1,34 @@
 var app = require("../app");
 
 
-app.service('textService', ['$resource', '$q', function ($resource, $q) {
-
-    //this.getHtml = function () {
+app.service('textService', ['$http', '$q', function ($http, $q) {
     //
-    //    return $resource("resource/template/:id.html", null, {
-    //        'get': {
-    //            method: 'GET',
-    //            isArray: false,
-    //            responseType: 'text'
-    //        }
-    //    });
+    //var contentJson = function () {
+    //    var promise = $resource('resource/content.json').get().$promise;
+    //
+    //    return promise
+    //        .then(function (resource) {
+    //            if (resource.success) {
+    //                return resource.data.content;
+    //            } else {
+    //                return $q.reject('not success');
+    //            }
+    //        });
     //
     //};
+
+    this.getHtml = function (fileAddress) {
+
+           return $http.get(fileAddress)
+               .then(function(response) {
+                   return response.data;
+               }, function(err){
+                   return err.data;
+               });
+
+
+    };
+
 
 
 
